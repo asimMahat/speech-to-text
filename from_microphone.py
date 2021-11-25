e1200721137b833fccdbffe967f3b3279a585d83
asimMahat111 <mahatasim123@gmail.com>
2021-11-25 19:18:48 +0545

    Testing for nepali language

--- from_microphone.py
+++ from_microphone.py
@@ -1,48 +1,67 @@
+# Importing speech recognition library
 import speech_recognition as sr
 
 # Initializing recognizer class
 r = sr.Recognizer()
 
 with sr.Microphone() as source :
-    print("Start talking")
-    audio_input = r.listen(source)
-    print("Stop")
+
+    user_input = int(input("Choose your preferred language \n 1. English  \n 2. Nepali \n "))
+    # print (type(user_input))
+
+    # This is for English language
+    if user_input == 1:
+        print("You have 10 seconds to speak")
+        r.adjust_for_ambient_noise(source) #adjusting for background noise
+        audio_input = r.listen(source,phrase_time_limit=10)
+        # print(type(audio_input))
+        # audio_input=r.record(source)
+        print("Stop")
 
     
-# Using exception handling to know if speech recognition API works or not
-    try :
-        #using google speech recognition 
-        text = r.recognize_google(audio_input)
+        # Using exception handling to know if speech recognition API works or not
+        try :
+            #using google speech recognition 
+            text = r.recognize_google(audio_input)
+            print("Converting audio into text")
+            # print(type(text))
+            print(text)
 
-        # For adding any other language ,in this case we are converting into french language
-        # text = r.recognize_google(audio_text, language ='fr-FR')
+            # Writing the result into the text file 
+            with open("microphone-results.txt", "w") as f:
+                f.write(text)
 
-        print("Converting audio into text")
-        print(text)
+        except :
+            print ('Sorry run again !!!')
+    
 
-    except :
-        print ('Sorry run again !!!')
+    # This is for Nepali language
+    elif user_input == 2:
+        print("You have 10 seconds to speak")
+        r.adjust_for_ambient_noise(source) #adjusting for background noise
+        audio_input = r.listen(source,phrase_time_limit=10)
+        # print(type(audio_input))
+        # audio_input=r.record(source)
+        print("Stop")
 
+    
+        # Using exception handling to know if speech recognition API works or not
+        try :
+            # using google speech recognition      
+            # In this case we are doing for nepali language
+            text = r.recognize_google(audio_input, language ='ne-NP')
+            print("Converting audio into text")
+            # print(type(text))
+            print(text)
 
-with open("microphone-results.raw", "wb") as f:
-    f.write(audio_input.get_raw_data())
+        # Writing the result into the text file 
+            with open("microphone-results.txt", "w") as f:
+                f.write(text)
 
-'''
+        except :
+            print ('Sorry run again !!!')
+    
+    else :
+        print("Only choose between '1' or '2' ")
+    
 
-# write audio to a RAW file
-with open("microphone-results.raw", "wb") as f:
-    f.write(audio.get_raw_data())
-
-# write audio to a WAV file
-with open("microphone-results.wav", "wb") as f:
-    f.write(audio.get_wav_data())
-
-# write audio to an AIFF file
-with open("microphone-results.aiff", "wb") as f:
-    f.write(audio.get_aiff_data())
-
-# write audio to a FLAC file
-with open("microphone-results.flac", "wb") as f:
-    f.write(audio.get_flac_data())
-
-'''
